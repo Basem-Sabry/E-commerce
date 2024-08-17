50c36590ad3f5b396ac9c5cb72adfd396e386772
Bassem Sabry <bsabry@devsolutions.team>
2024-08-17 12:24:17 +0300

    Handle Login State

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { SharedService } from '../../services/shared.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  loggedIn:boolean = false
+  constructor(private _shared : SharedService,private _router : Router) {
 
+  }
+  ngOnInit(): void {
+    this._shared.isLoggedIn.subscribe(res => {
+      if (res) {
+        console.log('reser',res)
+        this.loggedIn = true
+      }
+    })
+
+
+  }
+  logout(){
+    localStorage.clear()
+    this.loggedIn = false
+    this._shared.isLoggedIn.next(null)
+    this._router.navigate(['/'])
+  }
 }
